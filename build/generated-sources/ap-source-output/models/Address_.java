bdeb654cb38e98024a4b65a021f2647780112a32
Andrew <graduadu@gmail.com>
2021-02-10 10:12:48 +0200

    added save in database

---
+++ build/generated-sources/ap-source-output/models/Address_.java
@@ -0,0 +1,21 @@
+package models;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-02-10T09:47:57")
+@StaticMetamodel(Address.class)
+public class Address_ { 
+
+    public static volatile SingularAttribute<Address, String> date;
+    public static volatile SingularAttribute<Address, String> firstname;
+    public static volatile SingularAttribute<Address, String> address;
+    public static volatile SingularAttribute<Address, String> code;
+    public static volatile SingularAttribute<Address, String> city;
+    public static volatile SingularAttribute<Address, Integer> id;
+    public static volatile SingularAttribute<Address, String> title;
+    public static volatile SingularAttribute<Address, String> email;
+    public static volatile SingularAttribute<Address, String> lastname;
+
+}
